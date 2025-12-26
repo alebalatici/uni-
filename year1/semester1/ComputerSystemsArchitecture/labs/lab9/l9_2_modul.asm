eda2ad60d907197a3e99e9f932de291b306d7915
Alexandra Bălătici <alexandra.balatici@yahoo.com>
2025-12-26 15:04:53 +0200

    Update l9_2_modul.asm

--- year1/semester1/ComputerSystemsArchitecture/labs/lab9/l9_2_modul.asm
+++ year1/semester1/ComputerSystemsArchitecture/labs/lab9/l9_2_modul.asm
@@ -1,4 +1,4 @@
-//l9_2_main.c + l9_2_modul.asm 
+; l9_2_main.c + l9_2_modul.asm 
 global _adaugare_asm
 segment code use32 class=code
 _adaugare_asm:
@@ -17,4 +17,5 @@ _adaugare_asm:
     
     pop ebp 
     ret
+
 
