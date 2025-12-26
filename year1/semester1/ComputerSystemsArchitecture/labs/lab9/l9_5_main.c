cdf5a4ce9974145cee31da8cea88d4fe5504d9fd
Alexandra Bălătici <alexandra.balatici@yahoo.com>
2025-12-26 14:43:50 +0200

    Update and rename main1.c to l9_5_main.c

--- year1/semester1/ComputerSystemsArchitecture/labs/lab9/l9_5_main.c
+++ year1/semester1/ComputerSystemsArchitecture/labs/lab9/l9_5_main.c
@@ -1,24 +1,21 @@
+//l9_5_main.c + l9_5_modul.c 
 #include <stdio.h>
-
-// declarare functie ASM
+// declarare functie asm
 int asm_calcul(int a, int b, int c);
-
-// declarare functie C
+// declarare functie c
 void citire_abc(int* a, int* b, int* c);
-
 int main()
 {
     int a, b, c;
     int rez;
 
-    citire_abc(&a, &b, &c);   // trimitem ADRESELE
+    citire_abc(&a, &b, &c);   // trimitem adresele
 
     rez = asm_calcul(a, b, c);
 
     printf("%d\n", rez);
     return 0;
 }
-
 void citire_abc(int* a, int* b, int* c)
 {
     printf("Introduceti numarul a: ");
