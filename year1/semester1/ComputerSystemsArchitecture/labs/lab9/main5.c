fba81fc339f811f16f0c28d0c67c403dc61438ef
Alexandra Bălătici <alexandra.balatici@yahoo.com>
2025-12-26 14:49:32 +0200

    Add files via upload

---
+++ year1/semester1/ComputerSystemsArchitecture/labs/lab9/main5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+extern void adaugare_asm(int x, int sir[], int* len);
+void afisare_sir(int sir[], int len);
+
+int main()
+{
+	int sir[100];
+	int len = 0;
+	int x;
+	while (scanf("%d", &x) == 1)
+	{
+		adaugare_asm(x, sir, &len);
+	}
+	afisare_sir(sir, len);
+	return 0;
+}
+void afisare_sir(int sir[], int len)
+{	
+	printf("Sirul citit:\n");
+		for (int i = 0; i < len; i++)
+		{
+			printf("%d ", sir[i]);
+	}
+	printf("\n");
+}
---
+++ year1/semester1/ComputerSystemsArchitecture/labs/lab9/modul5.asm
@@ -0,0 +1,18 @@
+global _adaugare_asm
+segment code use32 class=code
+_adaugare_asm:
+    push ebp
+    mov ebp, esp 
+    
+    ;adaugare_asm(x, sir[], len)
+    mov eax, [ebp + 8] ;x
+    mov edx, [ebp + 12] ; sir
+    mov ecx, [ebp + 16] ; pointer len
+    
+    mov ebx, [ecx]
+    mov [edx + ebx * 4], eax ; sir[len] = x  
+    inc ebx 
+    mov [ecx], ebx 
+    
+    pop ebp 
+    ret
