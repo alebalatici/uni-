26353bb416b87afa64d4bb1c4d6d32ce726ada33
Alexandra Bălătici <alexandra.balatici@yahoo.com>
2026-01-16 14:48:36 +0200

    Add files via upload

--- year1/semester1/ComputerSystemsArchitecture/basics/perfect_square.asm
+++ year1/semester1/ComputerSystemsArchitecture/basics/perfect_square.asm
@@ -1,68 +1,47 @@
 bits 32 
 global start        
-extern exit
-extern scanf 
-extern printf 
+extern exit, printf, scanf                
 import exit msvcrt.dll   
-import scanf msvcrt.dll
-import printf msvcrt.dll  
+import printf msvcrt.dll
+import scanf msvcrt.dll  
 segment data use32 class=data
-    s resd 1
-    frm db "%d", 0 
-    frm_print_yes db "Este patrat perfect: %d", 0
-    frm_print_no db "Nu este patrat perfect: %d", 0
+    frm db "%s", 0 
+    frm_int db "%d", 0
+    n dd 0
+    yes db "%d este patrat perfect", 0  
+    no db "%d nu este patrat perfect", 0 
 segment code use32 class=code
     start:
-        push dword s 
-        push dword frm 
+        push dword n 
+        push dword frm_int
         call [scanf]
         add esp, 8
         
-        cmp [s], dword 0
-        jne not_zero
-        
-        push dword [s]
-        push dword frm_print_yes
-        call [printf]
-        add esp, 8
-        
-        jmp stop
-        
-        not_zero:
-            mov eax, 0 
-            parcurgere:
-                push eax 
-                mov edx, 0 
-                mul eax 
-                
-                cmp eax, [s] 
-                je final
-                
-                jg nu_e_sq
-                
-                pop eax 
-                inc eax 
-                jmp parcurgere
+        mov eax, 0 
+        perfect_square_loop:
+            push eax 
+            mov edx, 0 
+            mul eax 
+            cmp eax, [n]
+            je perfect_square 
+            jg not_perfect_square
+            pop eax 
+            inc eax 
+            jmp perfect_square_loop
             
-            nu_e_sq:
-            mov eax, 0 
+        perfect_square:
+            push dword [n]
+            push dword yes 
+            call [printf]
+            add esp, 8
+            jmp stop
             
-            final:
-                cmp eax, 0 
-                jne is_perfect_square
-                
-                push dword eax 
-                push dword frm_print_no 
-                call [printf]
-                add esp, 8
-                jmp stop 
+        not_perfect_square:
+            push dword [n]
+            push dword no 
+            call [printf]
+            add esp, 8             
             
-            is_perfect_square:
-                push dword eax 
-                push dword frm_print_yes
-                call [printf]
-                add esp, 8 
-                
         stop:
         push    dword 0     
-        call    [exit]     
+        call    [exit]    
