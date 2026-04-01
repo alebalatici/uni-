b5410e9ca637b6ca967dc84e3557b168e8e9287e
Alexandra Bălătici <alexandra.balatici@yahoo.com>
2026-04-01 21:43:05 +0300

    Delete year1/semester1/ProgrammingFundamentals/exam/Automobile directory

--- year1/semester1/ProgrammingFundamentals/exam/Automobile/main.py
+++
@@ -1,8 +0,0 @@
-from repository.repository_automobil import RepositoryFile
-from service.service_automobil import ServiceAutomobil
-from ui.console import Console
-
-repository = RepositoryFile("data/automobile.txt")
-service = ServiceAutomobil(repository)
-console = Console(service)
-console.run()
